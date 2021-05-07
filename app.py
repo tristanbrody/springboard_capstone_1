562188c9ce72390ed9756ab9bf3c58e1c1f0d88f
Tristan Brody <tristandavisbrody@gmail.com>
2021-05-06 22:05:56 -0700

    fixing uri

--- app.py
+++ app.py
@@ -79,6 +79,8 @@ login_manager.login_view = "login"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
     "DATABASE_URL", "SQLALCHEMY_DATABASE_URI"
 )
+if app.config.startswith("postgres://"):
+    app.config = app.config.replace("postgres://", "postgresql://", 1)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
